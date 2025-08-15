5e15a2374ddf111a99af7d544e313584b8a0a223
M-USMAN786 <youngboys920842@gmail.com>
2025-08-15 11:41:13 +0500

    Initial commit

---
+++ home/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+# class Product(models.Model):
+#     product_name=models.TextField(default='none',max_length=100)
+#     product_image=models.FileField(default=None)
+#     product_id=models.CompositePrimaryKey(default=None,unique=True)
