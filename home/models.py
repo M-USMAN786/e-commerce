21af4d81a9b139760d5c48849a31e4de16e4e1fa
M-USMAN786 <youngboys920842@gmail.com>
2025-08-19 09:49:13 +0500

    creating product named model

--- home/models.py
+++ home/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
-# class Product(models.Model):
-#     product_name=models.TextField(default='none',max_length=100)
-#     product_image=models.FileField(default=None)
-#     product_id=models.CompositePrimaryKey(default=None,unique=True)
+class Product(models.Model):
+    id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product_name= models.CharField(max_length=255)
+    product_image = models.ImageField(upload_to='products/', blank=True, null=True)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_category = models.CharField(max_length=100, blank=True, null=True)
+    product_description = models.TextField(blank=True, null=True)
+    
